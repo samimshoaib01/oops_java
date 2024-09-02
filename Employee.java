4954976e585785f0df3bd1ae02f18a4b0c0e27c8
samimshoaib01 <samimshoaib01@gmail.com>
2024-09-02 11:48:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,80 @@
+class Employee {
+ 
+ String name;
+ int employeeID;
+ double salary;
+ 
+ Employee(String name, int employeeID, double salary)
+ {
+ 	this.name=name;
+ 	this.employeeID = employeeID;
+ 	this.salary = salary;
+ 
+ }
+ 
+ displayDetails()
+	{
+          System.out.println("Name :"+name);
+          System.out.println("Employee Id :"+employeeID);
+          System.out.println("Salary :"+salary);
+          
+	}
+
+ }
+ 
+ 
+ class Manager extends Employee
+{
+	
+	int teamSize;
+
+Manager(String name,int employeeID, double salary,int teamSize)
+{
+	super(name,employeeID,salary);
+	this.teamSize = teamSize;
+}
+
+@Override
+displayDetails()
+	{
+          System.out.println("Name :"+name);
+          System.out.println("Employee Id :"+employeeID);
+          System.out.println("Salary :"+salary);
+          System.out.println("Team size :"+teamSize);
+          
+	}
+
+	
+
+}
+
+
+class Director extends Manager
+{
+	double budget;
+	Director(String name,int employeeID, double salary,int teamSize,double budget)
+{
+	super(name,employeeID,salary,teamSize);
+	this.budget = budget;
+}
+
+displayDetails()
+	{
+          System.out.println("Name :"+name);
+          System.out.println("Employee Id :"+employeeID);
+          System.out.println("Salary :"+salary);
+          System.out.println("Team size :"+teamSize);
+          System.out.println("Budget :"+budget);
+          
+	}
+	
+
+
+
+
+}
+
+int
+
+
+
